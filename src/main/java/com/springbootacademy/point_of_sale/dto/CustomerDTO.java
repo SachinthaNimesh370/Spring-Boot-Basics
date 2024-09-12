1f7b1f1e36b8c59bc40a523d51f5d1b956c86d22
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-12 12:38:16 +0530

    Create dto Class

--- src/main/java/com/springbootacademy/point_of_sale/dto/CustomerDTO.java
+++ src/main/java/com/springbootacademy/point_of_sale/dto/CustomerDTO.java
@@ -1,4 +1,97 @@
 package com.springbootacademy.point_of_sale.dto;
 
+
+import java.util.ArrayList;
+
 public class CustomerDTO {
+
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+    private double salary;
+    private ArrayList contactnumber;
+    private String nic;
+    private boolean active;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(int customerId, String customerName, String customerAddress, double salary, ArrayList contactnumber, String nic, boolean active) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.salary = salary;
+        this.contactnumber = contactnumber;
+        this.nic = nic;
+        this.active = active;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public ArrayList getContactnumber() {
+        return contactnumber;
+    }
+
+    public void setContactnumber(ArrayList contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", salary=" + salary +
+                ", contactnumber=" + contactnumber +
+                ", nic='" + nic + '\'' +
+                ", active=" + active +
+                '}';
+    }
 }
